d7b4e3349b28cb65a7c6b902eee2bccb55ba7f0f
Mustafa Simav <ms@msimav.net>
2013-10-03 00:57:35 +0300

    Better naming of fields of Edge

--- src/main/scala/Models.scala
+++ src/main/scala/Models.scala
@@ -4,13 +4,13 @@ object Models {
 	sealed trait GraphObject
 
 	case class RelationType(name: String) extends GraphObject
-	case class Edge(in: Node, out: Node, relationtype: RelationType) extends GraphObject
+	case class Edge(from: Node, to: Node, relationtype: RelationType) extends GraphObject
 	case class Node(id: String) extends GraphObject
 
 	implicit def str2node(value: String) = Node(value)
 	implicit def str2rel(value: String) = RelationType(value)
 	implicit def tup2edge(value: ((String, String), String)) = {
-		val ((in, rel), out) = value
-		Edge(Node(in), Node(out), RelationType(rel))
+		val ((from, rel), to) = value
+		Edge(Node(from), Node(to), RelationType(rel))
 	}
 }
