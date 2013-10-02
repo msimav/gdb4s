8a4a390b2e2b497c67a6e79d7f09471fae3daf8e
Mustafa Simav <ms@msimav.net>
2013-10-03 01:22:58 +0300

    Internal data changed to Symbol from String

--- src/main/scala/Models.scala
+++ src/main/scala/Models.scala
@@ -3,13 +3,13 @@ package ms.tobbetu.gdb4s;
 object Models {
 	sealed trait GraphObject
 
-	case class RelationType(name: String) extends GraphObject
+	case class RelationType(name: Symbol) extends GraphObject
 	case class Edge(from: Node, to: Node, relationtype: RelationType) extends GraphObject
-	case class Node(id: String) extends GraphObject
+	case class Node(id: Symbol) extends GraphObject
 
-	implicit def str2node(value: String) = Node(value)
-	implicit def str2rel(value: String) = RelationType(value)
-	implicit def tup2edge(value: ((String, String), String)) = {
+	implicit def str2node(value: Symbol) = Node(value)
+	implicit def str2rel(value: Symbol) = RelationType(value)
+	implicit def tup2edge(value: ((Symbol, Symbol), Symbol)) = {
 		val ((from, rel), to) = value
 		Edge(Node(from), Node(to), RelationType(rel))
 	}
