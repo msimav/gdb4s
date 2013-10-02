2f2e6e881a164c819fd10202b2307bb45c8b08ff
Mustafa Simav <ms@msimav.net>
2013-10-02 17:37:51 +0300

    implicit conversions added to Models package

--- src/main/scala/Models.scala
+++ src/main/scala/Models.scala
@@ -6,4 +6,11 @@ object Models {
 	case class RelationType(name: String) extends GraphObject
 	case class Edge(in: Node, out: Node, relationtype: RelationType) extends GraphObject
 	case class Node(id: String) extends GraphObject
+
+	implicit def str2node(value: String) = Node(value)
+	implicit def str2rel(value: String) = RelationType(value)
+	implicit def tup2edge(value: ((String, String), String)) = {
+		val ((in, rel), out) = value
+		Edge(Node(in), Node(out), RelationType(rel))
+	}
 }
