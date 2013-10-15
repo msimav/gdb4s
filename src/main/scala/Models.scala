26e6c4d4850aad8d376108cac2c7c10466eec1bd
Mustafa Simav <ms@msimav.net>
2013-10-15 19:11:05 +0300

    json serialization for Models added

--- src/main/scala/Models.scala
+++ src/main/scala/Models.scala
@@ -1,11 +1,34 @@
 package ms.tobbetu.gdb4s;
 
-object Models {
-	sealed trait GraphObject
+import spray.json._
+import DefaultJsonProtocol._
 
-	case class RelationType(name: Symbol) extends GraphObject
-	case class Edge(from: Node, to: Node, relationtype: RelationType) extends GraphObject
-	case class Node(id: Symbol) extends GraphObject
+object Models {
+	sealed trait GraphObject {
+		def asJson : String
+	}
+
+	object GraphObject {
+		def toEdge(source: String) = source.asJson.convertTo[Edge]
+		def toNode(source: String) = source.asJson.convertTo[Node]
+		def toRelationType(source: String) = source.asJson.convertTo[RelationType]
+	}
+
+	// spray-json implicits
+	implicit val relationTypeProtocol = jsonFormat1(RelationType)
+	implicit val nodeProtocol = jsonFormat1(Node)
+	implicit val edgeProtocol = jsonFormat3(Edge)
+
+	case class RelationType(name: Symbol) extends GraphObject {
+		def asJson = this.toJson.compactPrint
+	}
+	case class Edge(from: Node, to: Node, relationtype: RelationType) extends GraphObject {
+		def asJson = this.toJson.compactPrint
+	}
+	case class Node(id: Symbol) extends GraphObject {
+		def asJson = this.toJson.compactPrint
+	}
+
 
 	implicit def str2node(value: Symbol) = Node(value)
 	implicit def str2rel(value: Symbol) = RelationType(value)
