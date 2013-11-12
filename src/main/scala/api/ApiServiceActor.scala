ddf0b45270ed97ff8c3cb9efc48938da0b39df9b
Mustafa Simav <ms@msimav.net>
2013-11-12 18:06:09 +0200

    Working API for db and batch endpoints

--- src/main/scala/api/ApiServiceActor.scala
+++ src/main/scala/api/ApiServiceActor.scala
@@ -6,17 +6,18 @@ import reflect.ClassTag
 
 import spray.routing._
 import spray.json._
-import DefaultJsonProtocol._
-import spray.httpx.marshalling._
+import spray.httpx.SprayJsonSupport._
 import spray.http.{ HttpEntity, ContentTypes }
 
 import akka.actor.ActorRef
 import akka.pattern.ask
 import akka.util.Timeout
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 import ms.tobbetu.gdb4s.Models._
+import GraphJsonProtocol._
 import ms.tobbetu.gdb4s.core.Messages._
 
 class ApiServiceActor(backend: ActorRef) extends HttpServiceActor {
@@ -31,41 +32,80 @@ class ApiServiceActor(backend: ActorRef) extends HttpServiceActor {
 
   private implicit val timeout = Timeout(5 seconds)
 
-  // private implicit val marshaller = Marshaller.of[Set[Edge]](ContentTypes.application/json) { (value, contentType, ctx) =>
-  //     ctx.marshalTo(HttpEntity(contentType, value.toJson))
-  //   }
-
-  def receive = runRoute(pathPrefix("db") { dbRoute })
+  def receive = runRoute{
+    pathPrefix("db") { dbRoute } ~
+    pathPrefix("batch") { batchRoute }
+  }
 
   val dbRoute = get {
       path(Segments) {
         case Nil => reject
-        case from :: Nil => complete { queryBackend(Query(from, None, None)) }
-        case from :: rel :: Nil => complete { queryBackend(Query(from, None, rel)) }
-        case from :: rel :: to :: Nil => complete {
-          val reply = backend ? Query(from, to, rel)
-          for {
-            result <- reply.mapTo[Option[Edge]]
-          } yield (result.toString)
-        }
+        case from :: Nil => query(Query(from, None, None))
+        case from :: rel :: Nil => query(Query(from, None, rel))
+        case from :: rel :: to :: Nil => query(Query(from, to, rel))
         case _ => reject
       }
     } ~
     post {
       path(Segments) {
-        case from :: rel :: to :: Nil => complete {
-          val query = Add(Right(Symbol(from) -> Symbol(rel) -> Symbol(to)))
-          backend ! query
-          query.toString
+        case node :: Nil => complete {
+          val query = Add(Left(node))
+          (backend ? query).mapTo[Option[Node]]
         }
-        case _ => complete { "Hatali Post Istegi!" }
+        case from :: rel :: to :: Nil => complete {
+          val query = Add(Right(from -> rel -> to))
+          (backend ? query).mapTo[Option[Edge]]
+        }
+        case _ => reject
+      }
+    } ~
+    delete {
+      path(Segments) {
+        case node :: Nil => complete {
+          val query = Remove(Left(node))
+          (backend ? query).mapTo[Set[Edge]]
+        }
+        case from :: rel :: to :: Nil => complete {
+          val query = Remove(Right(from -> rel -> to))
+          (backend ? query).mapTo[Option[Edge]]
+        }
+        case _ => reject
+      }
+    } ~
+    put {
+      path(Segments) {
+        case oldNode :: Nil => entity(as[Node]) { newNode =>
+          complete {
+            val query = Update(Left(oldNode), Left(newNode))
+            (backend ? query).mapTo[Set[Edge]]
+          }
+        }
+        case from :: rel :: to :: Nil => entity(as[Edge]) { newEdge =>
+          complete {
+            val query = Update(Right(from -> rel -> to), Right(newEdge))
+            (backend ? query).mapTo[Option[Edge]]
+          }
+        }
+        case _ => reject
       }
     }
 
-    private def queryBackend[T](query: Query) = {
-          val reply = backend ? query
-          for {
-            result <- reply.mapTo[Set[Edge]]
-          } yield (result.toString)
+    val batchRoute =
+      post {
+        entity(as[List[Edge]]) { list =>
+          complete {
+            val success = for {
+              edge <- list
+            } yield (backend ? Add(Right(edge))).mapTo[Option[Edge]]
+            Future.sequence(success)
+          }
         }
+      }
+
+    def query(msg: Query) = msg match {
+      case Query(None, None, None) => reject
+      case Query(Some(_), Some(_), Some(_)) => complete { (backend ? msg).mapTo[Option[Edge]] }
+      case _ => complete { (backend ? msg).mapTo[Set[Edge]] }
+    }
+
 }
