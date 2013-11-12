0b0a06643fad968909508460a161796042beb92b
Mustafa Simav <ms@msimav.net>
2013-11-12 18:06:42 +0200

    Database path changed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,8 +15,8 @@ object Main extends App {
   // we need an ActorSystem to host our application in
   implicit val system = ActorSystem("gdb4s")
 
-  val path = java.io.File.createTempFile("gdb4s","test") // new File("/home/mustafa/workspace/scala/gdb4s/database")
-  val backend = system.actorOf(Props(classOf[FileSystemDatabaseWorkerActor], path), "backend")
+  val path = new File("/home/mustafa/.gdb4s/database")
+  val backend = system.actorOf(fsProps(path), "backend")
 
   // create and start our service actor
   val service = system.actorOf(Props(classOf[ApiServiceActor], backend), "rest-api")
